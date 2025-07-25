e823149bde6564a5b37c14a37cb6d5d56230797a
adam-bunce <adam.bunce11@gmail.com>
2025-07-24 21:52:13 -0400

    improve highways test

--- internal/scrape/highways_test.go
+++ internal/scrape/highways_test.go
@@ -2,6 +2,9 @@ package scrape
 
 import (
 	"fmt"
+	"golang.org/x/net/html"
+	"os"
+	"strings"
 	"testing"
 )
 
@@ -11,4 +14,29 @@ func TestGetHighwaysWeatherReport(t *testing.T) {
 		t.Fatal(err)
 	}
 	fmt.Println(report)
+}
+
+func TestProcessHighwaysMetarResponse(t *testing.T) {
+	documentRaw, err := os.ReadFile("testdata/happy_path/highways_cjy4.html")
+	if err != nil {
+		t.Fatal(err)
+	}
+	document, err := html.Parse(strings.NewReader(string(documentRaw)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	result, err := ProcessHighwaysMetarResponse(document)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedMetarCount := 6
+	expectedCamURLs := 3
+
+	if len(result.Metar) != expectedMetarCount {
+		t.Fatalf("Expected %d metars, got %d", expectedMetarCount, len(result.Metar))
+	}
+	if len(result.Cams) != expectedCamURLs {
+		t.Fatalf("Expected %d cams, got %d", expectedCamURLs, len(result.Cams))
+	}
 }
